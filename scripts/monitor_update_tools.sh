7457b6adf5440e0063f73d9e51fbb98c2162dd16
Akhn <vuvietanh201315@gmail.com>
2025-03-12 10:11:28 +0700

    No update

--- scripts/monitor_update_tools.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Checking and updating security tools..."
-
-echo "Updating OWASP ZAP..."
-wget -O zap.zip https://github.com/zaproxy/zaproxy/releases/latest/download/ZAP_2_11_1_Linux.tar.gz
-tar -xvzf zap.zip -C /opt/zap
-
-echo "Security tools have been updated."
